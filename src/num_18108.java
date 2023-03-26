7b145d86cad3566f0042c75e2aa087fea66bbab4
HwangSedong <saedongh5@naver.com>
2023-03-26 23:57:28 +0900

    23/3/26

---
+++ src/num_18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class num_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        int x = y - 543;
+
+        System.out.println(x);
+    }
+}
---
+++ src/num_2884.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class num_2884 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        if(m<)
+    }
+}
